63f17f6a393ea2c7f014295682a08262c242f4f7
v.giordano <v.giordano@almaviva.it>
2024-11-07 13:16:55 +0100

    feature/bidhub--docker-setup: Completed the docker setup by inserting image for backend and database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,36 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {
-  host: process.env.PG_HOST,
+// Configura l'URL del database per Docker o le variabili di ambiente individuali per l'ambiente di sviluppo
+const databaseUrl = process.env.DATABASE_URL ||
+  `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+const sequelize = new Sequelize(databaseUrl, {
   dialect: 'postgres',
-  port: process.env.PG_PORT,
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false, // Solo se usi SSL in produzione
+  }
 });
 
-sequelize.authenticate()
-  .then(() => console.log('Connessione al database avvenuta con successo!'))
-  .catch(err => console.error('Errore di connessione al database:', err));
+async function connectWithRetry(retries = 5, delay = 3000) {
+  while (retries) {
+    try {
+      await sequelize.authenticate();
+      console.log('Connessione al database avvenuta con successo!');
+      return;
+    } catch (err) {
+      retries -= 1;
+      console.error(`Errore di connessione al database: ${err.message}. Nuovo tentativo tra ${delay / 1000} secondi...`);
+      if (retries === 0) {
+        throw new Error('Impossibile connettersi al database dopo vari tentativi');
+      }
+      await new Promise((res) => setTimeout(res, delay));
+    }
+  }
+}
+
+connectWithRetry().catch(err => console.error(err.message));
 
 module.exports = sequelize;
